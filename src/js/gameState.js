fb058cac7c1ea18611dc9282c86d0bdaae798ce3
danishamov <danishamov@gmail.com>
2023-08-09 10:30:48 +0100

    create wizard with initial position on start

--- src/js/gameState.js
+++ src/js/gameState.js
@@ -1,6 +1,13 @@
 function initState() {
   const state = {
     player: "Alexander",
+    wizard: {
+      width: 82,
+      hight: 100,
+      startX: 0,
+      startY: 200,
+    },
+    keys: {},
   };
   return state;
 }
